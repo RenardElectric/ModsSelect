62aa21b8fe952b0465e502317bae067ed815ff72
RenardElectric <102425097+RenardElectric@users.noreply.github.com>
2022-12-01 21:10:23 +0100

    add requirements.txt

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-dependencies: sv_ttk,ntkutils,darkdetect,requests,keyboard,Pillow
-
 del ModsSelect.exe
 
 pyinstaller src/main.py --noconfirm --onefile --console --name "ModsSelect" --collect-data sv_ttk
